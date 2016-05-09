4587bf503fd761b070a7192f9dcf69f87058dcf8
adrijamil <adrijamil@hotmail.com>
2016-05-09 11:16:07 +0800

    'message'

---
+++ TestApp/MainWindow.xaml.vb
@@ -0,0 +1,89 @@
+﻿
+
+Class MainWindow
+
+    Public Parser As MyParser
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        Parser = New MyParser
+        Parser.ReadXML()
+
+    End Sub
+
+    Public Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        ' MessageBox.Show(String.Format("Hi {0}", Me.txtName.Text))
+        Parser = New MyParser
+        Parser.ReadXML()
+    End Sub
+    Public Sub OpenFileDialog_Inputs(sender As Object, e As RoutedEventArgs)
+        ' MessageBox.Show(String.Format("Hi {0}", Me.txtName.Text))
+
+        Dim dlg As New Microsoft.Win32.OpenFileDialog()
+
+        ' Set filter for file extension and default file extension 
+        dlg.DefaultExt = ".txt"
+        dlg.Filter = "Text documents (.txt)|*.txt"
+        dlg.Multiselect = True
+        ' Display OpenFileDialog by calling ShowDialog method 
+        Dim result As Nullable(Of Boolean) = dlg.ShowDialog()
+
+        ' Get the selected file name and display in a TextBox 
+        If result = True Then
+            ' Open document .
+            Dim filename As String = ""
+            Debug.Print(dlg.FileNames.Count)
+
+            For i = 0 To dlg.FileNames.Count - 1
+                filename = filename & dlg.FileNames(i) & Environment.NewLine
+            Next
+
+            InputFileList.Text = InputFileList.Text & filename
+        End If
+    End Sub
+    Public Sub OpenFileDialog_Output(sender As Object, e As RoutedEventArgs)
+        ' MessageBox.Show(String.Format("Hi {0}", Me.txtName.Text))
+
+        Dim dlg = New System.Windows.Forms.FolderBrowserDialog()
+
+        Dim btn As Button = sender
+
+        ' Display OpenFileDialog by calling ShowDialog method 
+        Dim result As Nullable(Of Boolean) = dlg.ShowDialog()
+        Debug.Print(btn.Name)
+        ' Get the selected file name and display in a TextBox 
+        If result = True Then
+            ' Open document .
+            Dim pathname As String = dlg.SelectedPath
+            If btn.Name = "btnBrowseOutputFolder" Then
+                txtOutputPath.Text = pathname
+            Else
+                txtReportPath.Text = pathname
+            End If
+
+        End If
+    End Sub
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        '   MessageBox.Show(String.Format("I'm loading"))
+        Parser = New MyParser
+        Parser.ReadXML()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As RoutedEventArgs)
+        If Parser Is Nothing Then
+            Parser = New MyParser
+        End If
+        Parser.ReadXML()
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+End Class
+
+
---
+++ TestApp/MyParser.vb
@@ -0,0 +1,86 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Xml
+
+
+Public Structure Component
+    Public Name As String
+    Public CAS As String
+    Public NBP As Double
+    Public StdLiqDens As Double
+    Public Mw As Double
+    Public Type As String
+End Structure
+
+Public Structure ComponentList
+    Public Name As String
+    Public Components() As Component
+End Structure
+
+Public Structure PropPack
+    Public Name As String
+    Public Type As String
+    Public ComponentList As ComponentList
+End Structure
+
+Public Class MyParser
+    'members
+    Public ComponentLists(0) As ComponentList
+
+    Public Sub New()
+        ' MessageBox.Show(String.Format("I instantiating myparser"))
+    End Sub
+
+    Public Sub ReadXML()
+        Dim output As StringBuilder = New StringBuilder()
+        'Dim myelement As XmlElement
+        Dim testbool As Boolean
+
+        Dim subtree As XmlReader
+
+        ' Create an XmlReader
+        'Using reader As XmlReader = XmlReader.Create(New StringReader(xmlString))
+
+        Dim reader As XmlReader = XmlReader.Create("XMLFile1.xml")
+
+        'reader.WhitespaceHandling = WhitespaceHandling.None
+        reader.ReadToFollowing("ComponentLists")
+        subtree = reader.ReadSubtree
+
+        testbool = reader.ReadToDescendant("ComponentList")
+        Do
+
+            subtree = reader.ReadSubtree
+            _getcomponentlists(subtree, ComponentLists(0))
+        Loop Until reader.ReadToFollowing("ReadToFollowing") = False
+
+        reader.Read()
+
+
+
+
+
+
+        'Do Until reader.NodeType = XmlNodeType.Element And reader.Name = "CaseDescription"
+        '    reader.Read()
+        'Loop
+        'Dim e As XElement = XElement.Load(reader)
+        'Console.WriteLine(e)
+        'End Using
+
+        MessageBox.Show("dhfd")
+    End Sub
+
+
+    Private Sub _getcomponentlists(reader As XmlReader, complist As ComponentList)
+        Dim comptree As String
+
+        reader.ReadToDescendant("ComponentListName")
+        reader.ReadToDescendant("Value")
+        complist.Name = reader.ReadElementContentAsString()
+        'Do
+        '    reader.ReadToDescendant("Component")
+
+        'Loop
+    End Sub
+End Class
